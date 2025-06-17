692e879381a6753ce85789802cbbe52b463125ca
Leonardo Pablo Arotoma <pabloarotomaleonardo@gmail.com>
2025-06-17 14:59:56 -0500

    Añadiendo la configuracion de la base de datos

--- backend/core/models.py
+++ backend/core/models.py
@@ -8,3 +8,25 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.name
+
+class AiChatSession(models.Model):
+    # "sesiones de seguimiento e inteligencia artificial"
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+class AiRequest(models.Model):
+    # representa solicitudes de ai
+    PENDING = 'pending'
+    RUNNING = 'running'
+    COMPLETED = 'complete'
+    FAILED = 'failed'
+    STATUS_OPTIONS = (
+        (PENDING, 'Pending'),
+        (RUNNING, 'Running'),
+        (COMPLETED, 'Completed'),
+        (FAILED, 'Failed'),
+    )
+    status = models.CharField(choices = STATUS_OPTIONS, default = PENDING)
+    response = models.JSONField(null = True, blank = True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
